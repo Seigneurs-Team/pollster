a13e6e020b9ad7f1c6733a3cc98fee310811253f
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2025-01-06 13:52:37 +0300

    создание опроса

--- src/app/main_page/views.py
+++ src/app/main_page/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, HttpRequest
 from databases.mysql_db import client_mysqldb
@@ -10,12 +12,13 @@ def request_on_main_page(requests):
     return render(requests, 'index.html', context={'all_objects': polls})
 
 
-def requests_on_get_polls(request, num_of_polls):
-    return JsonResponse({"list": client_mysqldb.get_polls(int(num_of_polls))})
+def requests_on_get_polls(request, num_of_polls=5):
+    polls = client_mysqldb.get_polls(int(num_of_polls))
+    return JsonResponse({"list": polls})
 
 
 def request_on_create_new_poll(request: HttpRequest):
-    print(list(request.FILES.items()))
-    poll: Poll = Poll(request.POST.get("description", ''), request.POST.get("name_of_poll", ''), request.POST.get("tags", ""), randint(1, 100000))
+    json_data = json.loads(request.body)
+    poll: Poll = Poll(json_data.get("description", ''), json_data.get("name_of_poll", ''), json_data.get("tags", ""), randint(1, 100000))
     result = client_mysqldb.create_poll(poll)
     return JsonResponse({"result": result})
