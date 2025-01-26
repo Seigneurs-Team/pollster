edf204fd520ef2b9b9648a54020920a161dd7d72
Luca324 <tanya.mosol@yandex.ru>
2025-01-26 21:27:40 +0300

    вход и регистрация
    страницы без стилей. реализована логика. осталось сделать запросы на сервер

--- src/app/common_static/scripts/sign_in_page.js
+++ src/app/common_static/scripts/sign_in_page.js
@@ -1,26 +1,29 @@
+// вход в аккаунт
+
+const host = 'http://127.0.0.1:8000';
+
 $('#loginForm').on('submit', function (event) {
     event.preventDefault(); // предотвращает стандартное поведение формы
 
     // Получаем данные из формы
     let login = $('input[name="login"]').val();
     let password = $('input[name="password"]').val();
-    
+
     // проверка данных формы: логин и пароль не пустые
-    if (login & password) {
+    if (login && password) {
 
         let dataJSON = JSON.stringify({
             login: login,
             password: password,
         })
         console.log('dataJSON', dataJSON)
-        // отправляем dataJSON на сервер. там проверяется, есть ли уже пользователь с таким логином: если да, то возвращается соответствующий код ошибки, и я в if его нахожу и пишу alert(`пользователь с таким логином уже существует`); если нет, то alert(`добро пожаловать, {login}`). если какая-то другая ошибка (т.е. два if, потом else), то пишу ошибка, попробуйте снова
+        // отправляем dataJSON на сервер. там проверяется существование аккаунта, правильность пароля, и возвращается соответструющий результат.
 
-        
 
         /*
                 const Http = new XMLHttpRequest();
         
-                Http.open("POST", host + "/create_poll", true); // TODO вместо create_poll запрос на создание аккаунта
+                Http.open("POST", host + "/create_poll", true); // TODO вместо create_poll запрос на вход в аккаунт
                 Http.setRequestHeader("Content-Type", "application/json");
                 Http.send(jsonPollData);
         
@@ -32,6 +35,7 @@ $('#loginForm').on('submit', function (event) {
                         window.location.href = '/'; // Перенаправление на домашнюю страницу
                     }
         */
+
     }
     else {
         console.log('логин и пароль не могут быть пустыми') // TODO пока что не alert. да и вообще лучше не alert, а в html выводить
