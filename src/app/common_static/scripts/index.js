af2aba09299f33816fc6fff1e3b2cd14f0136a49
Luca324 <tanya.mosol@yandex.ru>
2025-02-23 01:04:50 +0300

    pow2

--- src/app/common_static/scripts/index.js
+++ src/app/common_static/scripts/index.js
@@ -12,6 +12,7 @@ del_btns.on("click", function () {
 
 $(window).on("load", function () { getChallenge(); });
 
+// Шаг 1: Получить challenge от бэкенда
 function getChallenge() {
     // Установка куки
     document.cookie = "auth_sessionid=some_random_value; path=/;";
@@ -37,4 +38,24 @@ function getChallenge() {
             console.error('Error getting challenge:', Http.statusText);
         }
     };
+}
+
+// Шаг 2: Найти nonce
+function findProof(challenge) {
+    var count = 0;
+    var difficulty = challenge.count_of_bits;
+
+    function calculateHash() {
+        var stringForHash = `${challenge.version}:${challenge.count_of_bits}:${challenge.timestamp}:${challenge.resource}:${challenge.extension}:${challenge.random_string}:${count}`;
+        var hashValue = sha256(stringForHash); // Используй библиотеку для SHA-256
+
+        if (hashValue.startsWith('0'.repeat(difficulty))) {
+            console.log('Nonce found:', count);
+        } else {
+            count++;
+            setTimeout(calculateHash, 0); // Асинхронно продолжаем поиск
+        }
+    }
+
+    calculateHash();
 }
