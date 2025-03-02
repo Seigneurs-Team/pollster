d4f88980d1efcf7a0b9184d9693b8a363e1c20ac
Luca324 <tanya.mosol@yandex.ru>
2025-03-02 17:39:04 +0300

    ссылка профиля

--- src/app/passing_poll_page/views.py
+++ src/app/passing_poll_page/views.py
@@ -5,11 +5,13 @@ from Configs.Exceptions import NotFoundPoll
 
 
 def request_on_passing_poll_page(requests, poll_id):
+    # тут должна быть проверка, выполнен ли вход в аккаунт. если да, то user извлекается из БД (id, username и фото профиля в дальнейшем)
+    user = {'id' : 123, 'username' : "DikayaKakEnot"}
     print('poll_id: ', poll_id)
     # получение опроса по id
     try:
         poll = client_mysqldb.get_poll(poll_id)
         print(poll)
-        return render(requests, 'passing_poll_page.html', context={'poll': poll})
+        return render(requests, 'passing_poll_page.html', context={'user': user, 'poll': poll})
     except NotFoundPoll as _ex:
         return render(requests, 'NotFound.html')
