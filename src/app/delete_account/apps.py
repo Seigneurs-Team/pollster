f3f9dc5c65ba3bdac9907e451cda9c5b76bd5a00
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2025-03-09 21:43:00 +0300

    исправление путей

--- src/app/delete_account/apps.py
+++ src/app/delete_account/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DeleteAccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'delete_account'
+    name = 'app.delete_account'
