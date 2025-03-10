6394ded96d380aeb2c5e6f079bd999648953e2c9
Luca324 <tanya.mosol@yandex.ru>
2025-03-10 17:30:56 +0300

    fix sending results

--- src/app/common_static/scripts/passing_poll_page.js
+++ src/app/common_static/scripts/passing_poll_page.js
@@ -123,7 +123,7 @@ async function sendPassedPoll(results) {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         credentials: 'include', // Отправляем куки
-        body: results,
+        body: JSON.stringify(results),
     });
 
     const responseData = await response.json();
