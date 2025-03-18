4d065663a0e83b30444504e005c71fccbd3e80ec
Luca324 <tanya.mosol@yandex.ru>
2025-03-18 13:54:27 +0300

    тэги пользователя

--- src/app/common_static/scripts/profile_page.js
+++ src/app/common_static/scripts/profile_page.js
@@ -50,3 +50,42 @@ $(document).ready(function() {
     $("#your-polls-btn").addClass("active");
     $("#your-polls").show()
 })
+
+$(".delete-poll").on('click', async function () {
+    // Останавливаем всплытие события, чтобы предотвратить переход по ссылке
+    event.stopPropagation();
+    event.preventDefault();
+    console.log('deleting poll...');
+    const id = $(this).attr('data-poll');
+console.log(id)
+
+    const response = await fetch(`/delete_poll/${id}`, {
+        method: 'GET',
+        credentials: 'include', // Отправляем куки
+    });
+    const responseData = await response.json();
+    console.log('Ответ сервера:', responseData); // Выводим ответ сервера в консоль
+
+    if (!response.ok) {
+        console.error('Ошибка при удалении опроса:', responseData);
+        throw new Error('Ошибка при удалении опроса');
+    }
+
+    return responseData;
+})
+
+$('.tag').click(function() {
+    // Проверяем, где находится текущий элемент
+    if ($(this).parent().hasClass('not-selected-tags')) {
+        // Если элемент в .not-selected-tags, перемещаем его в .selected-tags
+        $(this).appendTo('.selected-tags');
+    } else if ($(this).parent().hasClass('selected-tags')) {
+        // Если элемент в .selected-tags, перемещаем его в .not-selected-tags
+        $(this).appendTo('.not-selected-tags');
+    }
+});
+
+// Фон шапки
+$(document).ready(function() {
+    $('header').css('background-image', 'url(' + $('header').data('background') + ')');
+});
