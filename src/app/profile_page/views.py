811714502d0e8461f1e0c0c8ac965d3f4debf36b
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2025-03-25 23:37:16 +0300

    fixup! отправка тегов для выбора пользователем

--- src/app/profile_page/views.py
+++ src/app/profile_page/views.py
@@ -16,5 +16,5 @@ def request_on_profile_page(requests, id_of_user):
     tags = {1: 'развлечения',  2: 'наука',  3: 'животные',  4: 'кухня',  5: 'искусство',  6: 'дети',  7: 'музыка',  8: 'кино и сериалы',  9: 'путешествия',  10: 'игры',  11: 'мода и стиль',  12: 'здоровье',  13: 'образование'}
 
     user = {'id': id_of_user, 'username': user_data[0], 'email': user_data[1], 'phone': user_data[2], 'date_of_birth': user_data[3], 'tags_of_user': tags_of_user, 'usersPolls': user_polls,  'completedPolls': pass_user_polls}
-    return render(requests, 'profile_page.html', context={'user': user, 'tags': tags})
+    return render(requests, 'profile_page.html', context={'user': user, 'tags': tags.items()})
 
